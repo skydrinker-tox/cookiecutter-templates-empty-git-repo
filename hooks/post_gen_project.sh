45a3a95e4cb7d59517ed37c1169f773af8dd8c78
skydrinker_tox <skydrinker_tox@hotmail.com>
2024-05-08 06:52:09 +0200

    📄 Added Optionnal LICENSE

--- hooks/post_gen_project.sh
+++ hooks/post_gen_project.sh
@@ -1,18 +1,16 @@
 #! /bin/bash
 
+{% if cookiecutter.license == "None" %}
+rm -f ./LICENSE
+{% endif %}
+
 {% if cookiecutter.init_git %}
 
 git config --global init.defaultBranch {{ cookiecutter.git_main_branch }}
 git config --global user.email {{ cookiecutter.git_email }}
 git config --global user.name {{ cookiecutter.git_username }}
 git init
-
 git add .
 git commit -a -m "Initial Commit."
-
-{% if cookiecutter.push_git %}
-git remote add origin {{ cookiecutter.git_repo_url }}
-git push -u origin {{ cookiecutter.git_main_branch }}
-{% endif %}
 
 {% endif %}
